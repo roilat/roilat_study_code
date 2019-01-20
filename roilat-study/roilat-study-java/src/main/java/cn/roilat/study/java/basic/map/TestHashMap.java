ede5615b88c9af902ee67012e2df5a12ba8fb401
roilat-D <123456>
2019-01-20 15:55:15 +0800

    123

--- roilat-study/roilat-study-java/src/main/java/cn/roilat/study/java/basic/map/TestHashMap.java
+++ roilat-study/roilat-study-java/src/main/java/cn/roilat/study/java/basic/map/TestHashMap.java
@@ -1,8 +1,28 @@
 package cn.roilat.study.java.basic.map;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class TestHashMap {
 
     public static void main(String[] args) {
+    	
+    	Map<String,String> map = new HashMap<String,String>();
+    	map.put("111","222");
+    	map.put("222","222");
+    	map.put("333","222");
+    	map.put("444","222");
+    	map.put("555","222");
+    	Iterator<Entry<String, String>> iter1 = map.entrySet().iterator();
+    	Iterator<Entry<String, String>> iter2 = map.entrySet().iterator();
+    	while(iter1.hasNext()){
+    		System.out.println(iter1.next());
+    		if(iter2.hasNext()){
+    			iter2.remove();
+    		}
+    	}
         testTableSizeFor(19);
     }
     public static void testTableSizeFor(int cap) {
