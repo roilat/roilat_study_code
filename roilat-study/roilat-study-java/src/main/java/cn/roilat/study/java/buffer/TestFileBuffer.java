e6b45806217d5f7530a026f5403a29375b67b84d
roilat <wb-dtw368035@antfin.com>
2019-04-02 19:34:54 +0800

    ''

---
+++ roilat-study/roilat-study-java/src/main/java/cn/roilat/study/java/buffer/TestFileBuffer.java
@@ -0,0 +1,39 @@
+package cn.roilat.study.java.buffer;
+
+import java.io.FileWriter;
+
+public class TestFileBuffer {
+
+    public static void main(String[] args) {
+        test1();
+        test2();
+    }
+
+    public static void test1() {
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("test1.txt");
+            for (int i = 0; i < 8193; i++) //写入超过8192个字节，即超过8k
+            {
+                fw.write('a');
+            }
+            fw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void test2() {
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("test2.txt");
+            for (int i = 0; i < 8193; i++) //写入超过8192个字节，即超过8k
+            {
+                fw.write('a');
+            }
+            fw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
